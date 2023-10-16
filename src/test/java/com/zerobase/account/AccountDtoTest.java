425793e9e70e2dd6bff6d9a646d0aeb881454d23
parktj <ekswn120@gmail.com>
2023-10-17 02:41:07 +0900

    lombok 기능 살펴보기

---
+++ src/test/java/com/zerobase/account/AccountDtoTest.java
@@ -0,0 +1,29 @@
+package com.zerobase.account;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDtoTest {
+
+
+    @Test
+    void accountDto() {
+        //given
+        AccountDto accountDto = new AccountDto(
+                "accountNumber",
+                "summer",
+                LocalDateTime.now()
+        );
+
+        //when
+        String accountNum = accountDto.getAccountNumber();
+
+        //then
+        System.out.println(accountNum);
+        System.out.println(accountDto.toString());
+
+    }
+}
