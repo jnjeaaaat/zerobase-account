1e7d2b85e8bf8f79ffd4b9cea8a7dd694b44ee3c
parktj <ekswn120@gmail.com>
2023-10-20 02:59:15 +0900

    계좌 생성 Controller test 코드

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
+-- resources/data.sql에 User 테이블 임의의 초기값 넣어줌
 INSERT INTO account_user(id, name, created_at, updated_at)
 VALUES (1, 'Pororo', now(), now());
 INSERT INTO account_user(id, name, created_at, updated_at)
--- src/test/java/com/zerobase/account/controller/AccountControllerTest.java
+++ src/test/java/com/zerobase/account/controller/AccountControllerTest.java
@@ -1,19 +1,28 @@
 package com.zerobase.account.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zerobase.account.domain.Account;
+import com.zerobase.account.dto.AccountDto;
+import com.zerobase.account.dto.CreateAccount;
 import com.zerobase.account.type.AccountStatus;
 import com.zerobase.account.service.AccountService;
 import com.zerobase.account.service.RedisTestService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
 
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,6 +39,34 @@ class AccountControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("계좌 생성 성공")
+    void successCreateAccount() throws Exception {
+        //given
+        given(accountService.createAccount(anyLong(), anyLong()))
+                .willReturn(AccountDto.builder()
+                        .userId(1L)
+                        .accountNumber("123456789")
+                        .registeredAt(LocalDateTime.now())
+                        .unRegisteredAt(LocalDateTime.now())
+                        .build());
+
+        //when
+        //then
+        mockMvc.perform(post("/account")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new CreateAccount.Request(1L, 100L)
+                )))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId").value(1))
+                .andExpect(jsonPath("$.accountNumber").value("123456789"))
+                .andDo(print());
+    }
 
     @Test
     void successGetAccount() throws Exception {
