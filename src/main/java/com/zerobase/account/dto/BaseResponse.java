bb076dd68bb4197fea15226dcb77c7bd43284ac4
parktj <ekswn120@gmail.com>
2023-10-22 21:43:51 +0900

    BaseResponse successCode 추가

--- src/main/java/com/zerobase/account/dto/BaseResponse.java
+++ src/main/java/com/zerobase/account/dto/BaseResponse.java
@@ -10,16 +10,17 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"isSuccess", "message", "result"})
+@JsonPropertyOrder({"isSuccess", "successCode", "message", "result"})
 public class BaseResponse<T> {//BaseResponse 객체를 사용할때 성공, 실패 경우
     @JsonProperty("isSuccess")
-    private final Boolean isSuccess;
+    private final Boolean isSuccess = true;
+    private final SuccessCode successCode;
     private final String message;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private T result;
 
     public BaseResponse(SuccessCode successCode, T result) {
-        this.isSuccess = successCode.isSuccess();
+        this.successCode = successCode;
         this.message = successCode.getDescription();
         this.result = result;
     }
